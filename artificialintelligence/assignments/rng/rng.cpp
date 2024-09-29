533748c3bd1a6097df6d47968bb6afc589f77f3a
tolstenko <tolstenko@users.noreply.github.com>
2024-09-29 22:18:22 +0000

    deploy: 6eba45c437a54c01b6bbb3ce089a0b9288297036

---
+++ artificialintelligence/assignments/rng/rng.cpp
@@ -0,0 +1,16 @@
+// add your imports here
+#include <fstream>
+#include <iostream>
+#include <istream>
+const std::string TEST_FOLDER = "\\tests\\";
+unsigned int xorShift(unsigned int seed, int r1, int r2);
+int main(){
+  // code here
+  unsigned int seed, N, min, max;
+  std::cin >> seed >> N >> min >> max;
+}
+//The purpose of this function is to take the number and xor shift it to output a pseudo-random number
+    unsigned int xorShift(unsigned int seed, int r1, int r2)
+{
+
+}
